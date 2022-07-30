37bcae09ac4ca50e3a1aba505d95b1bf7d03843e
Sherydan <tobar.luis.c@gmail.com>
2022-07-30 13:10:03 -0400

    added basic design

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+let txtCurrentDate = document.querySelector("#currentDate")
+
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {}
+};
+
+const myChart = new Chart(
+    document.querySelector("#currencyChart"), config
+);
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    txtCurrentDate.innerHTML = moment().format("dddd, MMMM, D")
+});
+
