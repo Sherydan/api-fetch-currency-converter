1b5b3722e98450681fead1d639d77dbdb917683f
Sherydan <tobar.luis.c@gmail.com>
2022-07-30 17:35:50 -0400

    added async function to fetch mindicador api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,47 @@
 let txtCurrentDate = document.querySelector("#currentDate")
+let txtChileanCLP = document.querySelector("txtChileanCLP")
+let dropdownCurrencyes = document.querySelector("#dropdownCurrencyes")
+let txtConvertedValue = document.querySelector("#txtConvertedValue")
+
+const convertCurrency = async(valueToConvert = 1, currencyId = "dolar") => {
+    try {
+        response = await fetch(`https://mindicador.cl/api`)
+        const mindicador = await response.json()
+
+        if (response.status !== 200) {
+            throw new Error(response.statusText);
+        } else {
+            console.log(mindicador.dolar.valor)
+            let convertedValue = (1 / mindicador.dolar.valor) * valueToConvert
+            console.log(convertedValue.toFixed(4))
+            
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+convertCurrency();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const labels = [
     'January',
