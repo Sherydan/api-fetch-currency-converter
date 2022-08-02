681aecd2bb35bee7bf249569a519a2ba7454946b
Sherydan <tobar.luis.c@gmail.com>
2022-08-02 02:52:52 -0400

    added functionallity to my page, now it can convert currency and show chart depending on the currency selected

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,82 +1,132 @@
-let txtCurrentDate = document.querySelector("#currentDate")
-let txtChileanCLP = document.querySelector("txtChileanCLP")
-let dropdownCurrencyes = document.querySelector("#dropdownCurrencyes")
-let txtConvertedValue = document.querySelector("#txtConvertedValue")
+let txtCurrentDate = document.querySelector("#currentDate");
+let txtChileanCLP = document.querySelector("#txtChileanCLP");
+let dropdownCurrencyes = document.querySelector("#dropdownCurrencyes");
+let txtConvertedValue = document.querySelector("#txtConvertedValue");
+let txtCurrentCurrency = document.querySelector("#txtCurrentCurrency");
+let btnConvert = document.querySelector("#btnConvert");
+let txtCurrentValue = document.querySelector("#txtCurrentValue");
 
-const convertCurrency = async(valueToConvert = 1, currencyId = "dolar") => {
-    try {
-        response = await fetch(`https://mindicador.cl/api`)
-        const mindicador = await response.json()
+const convertCurrency = async (valueToConvert, currencyId = "dolar") => {
+  try {
+    response = await fetch(`https://mindicador.cl/api/${currencyId}`);
+    const mindicador = await response.json();
 
-        if (response.status !== 200) {
-            throw new Error(response.statusText);
-        } else {
-            console.log(mindicador.dolar.valor)
-            let convertedValue = (1 / mindicador.dolar.valor) * valueToConvert
-            console.log(convertedValue.toFixed(4))
-            
-        }
-    } catch (error) {
-        console.log(error)
+    if (response.status !== 200) {
+      throw new Error(response.statusText);
+    } else {
+      let convertedValue = (1 / mindicador.serie[0].valor) * valueToConvert;
+      return convertedValue.toFixed(5);
     }
-}
-
-convertCurrency();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-];
-
-const data = {
-    labels: labels,
-    datasets: [{
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-    }]
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const config = {
-    type: 'line',
-    data: data,
-    options: {}
-};
-
-const myChart = new Chart(
-    document.querySelector("#currencyChart"), config
-);
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    txtCurrentDate.innerHTML = moment().format("dddd, MMMM, D")
+// convert currency when btnConvert is clicked
+btnConvert.addEventListener("click", async () => {
+  txtConvertedValue.innerHTML = await convertCurrency(
+    txtChileanCLP.value,
+    dropdownCurrencyes.value
+  );
 });
 
+// change txtCurrentCurrency when dropdownCurrencyes is changed
+dropdownCurrencyes.addEventListener("change", async () => {
+  txtCurrentCurrency.innerHTML = dropdownCurrencyes.value;
+  txtConvertedValue.innerHTML = await convertCurrency(
+    txtChileanCLP.value,
+    dropdownCurrencyes.value
+  );
+  fillChart(dropdownCurrencyes.value);
+});
+
+// convert currency when txtChileanCLP is changed
+txtChileanCLP.addEventListener("change", async () => {
+  txtConvertedValue.innerHTML = await convertCurrency(
+    txtChileanCLP.value,
+    dropdownCurrencyes.value
+  );
+});
+
+// change txtCurrentValue when txtChileanCLP is changed
+txtChileanCLP.addEventListener("change", () => {
+  let value = parseInt(txtChileanCLP.value);
+  txtCurrentValue.innerHTML = value.toLocaleString("es-CL", {
+    style: "currency",
+    currency: "CLP",
+  });
+});
+
+const fillChart = async (selectedCurrency = "dolar") => {
+  try {
+    response = await fetch(`https://mindicador.cl/api/${selectedCurrency}`);
+    const mindicador = await response.json();
+
+    if (response.status !== 200) {
+      throw new Error(response.statusText);
+    } else {
+      // get first 10 days of serie
+      let serie = mindicador.serie.slice(0, 10);
+      let dates = serie.map((item) => item.fecha);
+      let values = serie.map((item) => item.valor);
+      // convert dates values to locale string
+      dates = dates.map((date) => new Date(date).toLocaleDateString("es-CL"));
+
+      // create an array of objects with dates and values
+      let data = [];
+      for (let i = 0; i < dates.length; i++) {
+        data.push({
+          date: dates[i],
+          value: values[i],
+        });
+      }
+      console.log(data);
+      createChart(data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+function createChart(chartData) {
+  console.log(chartData);
+  // invert chartData
+  chartData.reverse();
+
+  const labels = chartData.map((item) => item.date);
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: chartData.map((item) => item.value),
+      },
+    ],
+  };
+
+  const config = {
+    type: "line",
+    data: data,
+    options: {},
+  };
+
+  // destroy canvas if it exists
+  if (document.getElementById("currencyChart")) {
+    document.getElementById("currencyChart").remove();
+  }
+
+  // create canvas
+  const canvas = document.createElement("canvas");
+  canvas.id = "currencyChart";
+
+  document.querySelector("#chart").appendChild(canvas);
+  const myChart = new Chart(document.querySelector("#currencyChart"), config);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  txtCurrentDate.innerHTML = moment().format("dddd, MMMM, D");
+  fillChart();
+});
